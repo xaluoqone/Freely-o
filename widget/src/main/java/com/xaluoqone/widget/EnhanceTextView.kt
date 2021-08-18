13912201c709e1ca54ddcc1358fe40902fa018f6
xaluoqone <1617321643@qq.com>
2021-08-18 11:02:57 +0800

    完善EnhanceTextView功能，添加ViewProperty

--- widget/src/main/java/com/xaluoqone/widget/EnhanceTextView.kt
+++ widget/src/main/java/com/xaluoqone/widget/EnhanceTextView.kt
@@ -7,47 +7,95 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.MotionEvent
 import androidx.appcompat.widget.AppCompatTextView
-import androidx.core.graphics.toColorInt
 
-class EnhanceTextView(context: Context, attrs: AttributeSet?) : AppCompatTextView(context, attrs) {
+class EnhanceTextView(context: Context, attrs: AttributeSet? = null) :
+    AppCompatTextView(context, attrs) {
     private val mPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private var isPress = false
-        set(value) {
-            if (field != value) {
-                field = value
-                invalidate()
-            }
-        }
-    var cornerRadius: Float
-    var backgroundValue: Int
-    var pressBackgroundValue: Int
+    private var isPress by viewPropertyOf(false)
+    var cornerRadius by viewPropertyOf(0f)
+    var backgroundValue by viewPropertyOf(0)
+    var pressBackgroundValue by viewPropertyOf(0)
+    var backgroundStroke by viewPropertyOf(0)
+    var pressBackgroundStroke by viewPropertyOf(0)
+    var backgroundStyle by viewPropertyOf(Paint.Style.FILL)
+    var pressBackgroundStyle by viewPropertyOf(Paint.Style.FILL)
+    var textColorValue by viewPropertyOf(0)
+    var pressTextColorValue by viewPropertyOf(0)
+    var backgroundStrokeWidth by viewPropertyOf(0f)
 
     init {
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.EnhanceTextView)
         cornerRadius = typedArray.getDimension(R.styleable.EnhanceTextView_cornerRadius, 0f)
-        backgroundValue = typedArray.getColor(
-            R.styleable.EnhanceTextView_backgroundValue,
-            "#00000000".toColorInt()
-        )
-        pressBackgroundValue = typedArray.getColor(
-            R.styleable.EnhanceTextView_pressBackgroundValue,
-            "#00000000".toColorInt()
-        )
+        backgroundValue = typedArray.getColor(R.styleable.EnhanceTextView_backgroundValue, 0)
+        pressBackgroundValue =
+            typedArray.getColor(R.styleable.EnhanceTextView_pressBackgroundValue, 0)
+        backgroundStroke = typedArray.getColor(R.styleable.EnhanceTextView_backgroundStroke, 0)
+        pressBackgroundStroke =
+            typedArray.getColor(R.styleable.EnhanceTextView_pressBackgroundStroke, 0)
+        backgroundStyle =
+            getStyleByAttrStr(typedArray.getInt(R.styleable.EnhanceTextView_backgroundStyle, 1))
+        pressBackgroundStyle =
+            getStyleByAttrStr(
+                typedArray.getInt(
+                    R.styleable.EnhanceTextView_pressBackgroundStyle,
+                    1
+                )
+            )
+        textColorValue = typedArray.getColor(R.styleable.EnhanceTextView_textColorValue, 0)
+        pressTextColorValue =
+            typedArray.getColor(R.styleable.EnhanceTextView_pressTextColorValue, 0)
+        backgroundStrokeWidth =
+            typedArray.getDimension(R.styleable.EnhanceTextView_backgroundStrokeWidth, 0f)
         typedArray.recycle()
     }
 
+    private fun getStyleByAttrStr(attrStr: Int?) =
+        when (attrStr) {
+            1 -> Paint.Style.FILL
+            2 -> Paint.Style.STROKE
+            3 -> Paint.Style.FILL_AND_STROKE
+            else -> Paint.Style.FILL
+        }
+
     override fun onDraw(canvas: Canvas) {
-        mPaint.color = if (isPress) pressBackgroundValue else backgroundValue
-        canvas.drawRoundRect(
-            0f,
-            0f,
-            width.toFloat(),
-            height.toFloat(),
+        mPaint.style = if (isPress) pressBackgroundStyle else backgroundStyle
+        mPaint.strokeWidth = backgroundStrokeWidth
+        when (mPaint.style!!) {
+            Paint.Style.STROKE -> {
+                mPaint.color = if (isPress) pressBackgroundStroke else backgroundStroke
+                canvas.drawBg(width, height, mPaint.strokeWidth / 2)
+            }
+            Paint.Style.FILL -> {
+                mPaint.color = if (isPress) pressBackgroundValue else backgroundValue
+                canvas.drawBg(width, height, 0f)
+            }
+            Paint.Style.FILL_AND_STROKE -> {
+                val offset = mPaint.strokeWidth / 2
+                mPaint.color = if (isPress) pressBackgroundStroke else backgroundStroke
+                mPaint.style = Paint.Style.STROKE
+                canvas.drawBg(width, height, offset)
+                mPaint.color = if (isPress) pressBackgroundValue else backgroundValue
+                mPaint.style = Paint.Style.FILL
+                canvas.drawBg(width, height, offset)
+            }
+        }
+        super.onDraw(canvas)
+    }
+
+    private fun Canvas.drawBg(
+        bgWidth: Int,
+        bgHeight: Int,
+        drawOffset: Float
+    ) {
+        drawRoundRect(
+            drawOffset,
+            drawOffset,
+            bgWidth - drawOffset,
+            bgHeight - drawOffset,
             cornerRadius,
             cornerRadius,
             mPaint
         )
-        super.onDraw(canvas)
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -55,11 +103,22 @@ class EnhanceTextView(context: Context, attrs: AttributeSet?) : AppCompatTextVie
         super.onTouchEvent(event)
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                if (pressBackgroundValue != 0 && backgroundValue != 0) {
+                if (((pressBackgroundValue != 0 || pressBackgroundStroke != 0) &&
+                            (backgroundValue != 0 || backgroundStroke != 0)) ||
+                    (pressTextColorValue != 0 && textColorValue != 0)
+                ) {
+                    if (pressTextColorValue != 0) {
+                        setTextColor(pressTextColorValue)
+                    }
                     isPress = true
                 }
             }
-            MotionEvent.ACTION_UP -> isPress = false
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                if (textColorValue != 0) {
+                    setTextColor(textColorValue)
+                }
+                isPress = false
+            }
         }
         return true
     }
